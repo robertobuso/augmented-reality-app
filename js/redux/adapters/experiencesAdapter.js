4ec9175c2e715e28c4ce9b13d8c28ed5b26bb4ae
robertobuso <robertobuso@gmail.com>
2018-10-14 14:28:40 -0400

    first on sunday

--- js/redux/adapters/experiencesAdapter.js
+++ js/redux/adapters/experiencesAdapter.js
@@ -2,9 +2,4 @@ export const fetchExperiences = () => {
 
   return fetch('https://glacial-peak-72949.herokuapp.com/api/v1/experiences')
     .then(r => r.json())
-  // .catch(function (err) {
-  //       debugger
-  //       return err;
-  //     });
-
 }
